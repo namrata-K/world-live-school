3bb24d4f5bd39ee96ecb30443e7050cf9d115a79
Priya Gupta <priyag20061999@gmail.com>
2019-03-02 17:23:50 +0530

    merge

--- xyz.php
+++ xyz.php
@@ -17,15 +17,13 @@
 <html>
 
 <head>
-<<<<<<< HEAD
-=======
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
->>>>>>> e8e1bf810e867c499a34a3a85261f3306ebb7fd4
+
 
     <title>Student</title>
     
